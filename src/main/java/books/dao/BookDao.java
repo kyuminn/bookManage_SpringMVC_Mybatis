ee10db9f8081f08d684f8a01cd4e569a25750794
unknown <starkyumin0622@gmail.com>
2021-12-28 09:55:05 +0900

    add desc for dao interface

--- src/main/java/books/dao/BookDao.java
+++ src/main/java/books/dao/BookDao.java
@@ -5,6 +5,8 @@ import java.util.List;
 import books.vo.BookVo;
 import books.vo.Paging;
 
+// 확장성을 위한 인터페이스 구현
+// 나중에 BookDao의 수정이 이루어져야 할 때 인터페이스는 수정하지 않고, 구현하는 부분만 수정하면 됨
 public interface BookDao {
 	public abstract void regist(BookVo vo);
 	public List<BookVo> list(Paging page);
