d8b7f746678be27d0e7b380495b3d7ef4c043195
unknown <starkyumin0622@gmail.com>
2021-12-29 19:28:07 +0900

    add desc of mvc pattern

--- src/main/java/books/controller/BookController.java
+++ src/main/java/books/controller/BookController.java
@@ -10,13 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import books.exception.DuplicateIsbnException;
 import books.exception.UnAttachedFileException;
@@ -30,7 +30,9 @@ public class BookController {
 	@Autowired
 	private BookService bookService;
 	
-	@RequestMapping(value="/books/add" ,method=RequestMethod.GET)
+	//@RequestMapping(value="/books/add" ,method=RequestMethod.GET)
+	// 또는 동일한 표현식으로
+	@GetMapping("/books/add")
 	public String regist(@ModelAttribute("vo")BookVo vo) {
 		return "/books/regForm";
 	}
